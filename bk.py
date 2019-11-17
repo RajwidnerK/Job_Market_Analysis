d3c1b08cfb67f24baeb39498248539646b00874a
RajwidnerK <56404831+RajwidnerK@users.noreply.github.com>
2019-11-16 19:53:27 -0500

    Changed the API

--- bk.py
+++ bk.py
@@ -8,22 +8,30 @@ import simplejson as json
 
 app = Flask(__name__)
 
-@app.route("/flaskJobs/<query>/<location>")
+
+@app.route("/")
 def jobDetails():
-    """
-    Parameters:
-        query: Indeed query keyword such as 'Data Scientist'
-        num_pages: Number of search results needed
-        location: location to search for
-    Returns:
-        Json
-    """
-    # Convert the queried title to Indeed format
-    query = '+'.join(query.lower().split())
-    jsonData = get_data(query, 3, location)
+    queries = ["software engineer","data scientist", "machine learning engineer", "data engineer"]
+    while True: 
+        query = input("Please enter the title to scrape data for: \n").lower()
+        if query in queries:
+            break
+        else:
+            print("Invalid title! Please try again.")
+
+    while True:
+        num_pages = input("Please enter the number of pages needed (integer only): \n")
+        try:
+            num_pages = int(num_pages)
+            break
+        except:
+            print("Invalid number of pages! Please try again.")
+
+    jsonData = get_data(query, num_pages, location='Windsor')
     return jsonData
 
-def get_data(query, num_pages, location):
+
+def get_urls(query, num_pages, location):
     #Author Rajwinder
     """
     Get all the job posting URLs resulted from a specific search.
@@ -40,7 +48,7 @@ def get_data(query, num_pages, location):
     sort_by = 'date'          # sort by data
     start_from = '&start='    # start page number  
 
-    for page in range(1,3):
+    for page in range(1,10):
         page = (page-1) * 10  
         # get full url 
         url = "%s%s%s%d" % (base_url, sort_by, start_from, page) 
@@ -50,9 +58,11 @@ def get_data(query, num_pages, location):
         jobList = []
         soup1 = BeautifulSoup(source,'lxml')
         for div2 in soup1.find_all('div', class_='jobsearch-SerpJobCard unifiedRow row result'):
+            #job_div = div2.find(class_='sjcl')
             job_title_div = div2.find(class_='title')
             job_location_div = div2.find(class_='location accessible-contrast-color-location')
             job_title = job_title_div.find('a').text
+            #company_name = job_div.find('div').text
             job_location = job_location_div.text
             geolocator = Nominatim(user_agent="your Google Key...")
             location = geolocator.geocode(job_location)
@@ -67,6 +77,20 @@ def get_data(query, num_pages, location):
             # convert to json data
             jsonStr = json.dumps(jobList)
     return jsonify(Jobs=jsonStr)
+
+def get_data(query, num_pages, location='Windsor'):
+    """
+    Parameters:
+        query: Indeed query keyword such as 'Data Scientist'
+        num_pages: Number of search results needed
+        location: location to search for
+    Returns:
+        postings_dict: Python dict including all posting data
+    """
+    # Convert the queried title to Indeed format
+    query = '+'.join(query.lower().split())
+    urls = get_urls(query, num_pages, location)
+    return urls
 
 # If script is run directly, we'll take input from the user
 if __name__ == "__main__":
