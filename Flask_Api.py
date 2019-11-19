5aba365b8f71da0aa0da2fe786b284da062d03ae
RajwidnerK <56404831+RajwidnerK@users.noreply.github.com>
2019-11-19 12:57:27 -0500

    Update Flask_Api.py
    Add the Crime Data and Job desc, address URL in the job detail JSON

--- Flask_Api.py
+++ Flask_Api.py
@@ -4,50 +4,61 @@ import requests
 from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
 import simplejson as json
+import regex
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def jobDetails():
     #Author Rajwinder
     # Collect and parse first page
     page = requests.get('https://ca.indeed.com/software-jobs')
-    # https://ca.indeed.com/viewjob?cmp=Adamson-Systems-Engineering&t=Software+Engineer&jk=02e60387021269b5&sjdu=vQIlM60yK_PwYat7ToXhk9I8NiHOQ-7v2o66QU9DKp25gg8ltlYMwsB723eeJewasQRwRpNzkHw5l1vcLi0knw&tk=1dmp4geja4ntl800&adid=306745201&pub=4a1b367933fd867b19b072952f68dceb&vjs=3
-    #page = requests.get('https://ca.indeed.com/viewjob?cmp=Adamson-Systems-Engineering&t=Software+Engineer&jk=02e60387021269b5&sjdu=vQIlM60yK_PwYat7ToXhk9I8NiHOQ-7v2o66QU9DKp25gg8ltlYMwsB723eeJewasQRwRpNzkHw5l1vcLi0knw&tk=1dmp4geja4ntl800&adid=306745201&pub=4a1b367933fd867b19b072952f68dceb&vjs=3
-
+    crimePage = requests.get("https://www.worldatlas.com/articles/most-dangerous-cities-in-canada.html")
+    crimeData = BeautifulSoup(crimePage.text, 'html.parser')
+    table = crimeData.find("table")
+    
+    #output_rows = []
+    output_row = []
+    for table_row in table.findAll('tr'):
+        columns = table_row.findAll('td')
+        
+        for column in columns:
+            output_row.append(column.text)
+            print(column.text)
+            print("*****************")
+            #output_rows.append(output_row)
+    
     soup = BeautifulSoup(page.text, 'html.parser')
 
     #Author Rajwinder# Pull all text from the BodyText div
-    #artist_name_list = soup.find(class_='summary')
     job_div = soup.find(class_='sjcl')
     job_title_div = soup.find(class_='title')
+    job_desc_div = soup.find(class_="summary")
+    job_desc = job_desc_div.text
     job_location_div = soup.find(class_='location accessible-contrast-color-location')
-
-    # Pull text from all instances of <a> tag within BodyText div
-    artist_name_list_items = job_div.find_all('span')
-
-    # Pull text from all instances of <a> tag within BodyText div
     job_title = job_title_div.find('a').text
-
-    # Pull text from all instances of <a> tag wiartist_name_listthin BodyText div
+    job_link = job_title_div.find('a').get('href')
     company_name = job_div.find('div').text
-
     job_location = job_location_div.text
-
-    
     geolocator = Nominatim(user_agent="your Google Key...")
     location = geolocator.geocode(job_location)
     lng = location.longitude
     lat = location.latitude
 
-    # Create for loop to print out all artists' names
-    print(job_title)
-    print(company_name)
-    print(job_location)
+    # print job details
+    print(output_row)
+    print('JOB TITLE --- '+job_title)
+    print('COMPANY NAME --- '+company_name)
+    print('JOB LOCATION--- '+job_location)
     print(lng)
     print(lat)
+    print('JOB LINK --- '+job_link)
+    print('JOB DESC --- '+job_desc)
     print("Success!")
-    # Initialize a employee list
-    return jsonify({'lng':lng,'lat':lat,'jobs':job_title,'open':'false' })
+    # Returns False as x is False 
+    x = False
+    
+    # Initialize a employee list JSON
+    return jsonify({'lng':lng,'lat':lat,'jobs':job_title,'url':'https://ca.indeed.com/'+job_link,'desc':job_desc,'address':job_location,'crimeData':output_row,'open':bool(x)})
 
 if __name__ == '__main__':
      app.run(port=5002)
