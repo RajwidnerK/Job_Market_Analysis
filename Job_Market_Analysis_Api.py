0316ee45c67ba87fdc4de209e519fc83cf79a0c5
RajwidnerK <56404831+RajwidnerK@users.noreply.github.com>
2019-11-19 13:04:36 -0500

    Update Job_Market_Analysis_Api.py
    Adding the CrimeData, Job Desc, Job Link,  Job address to the Job Detail JSON

--- Job_Market_Analysis_Api.py
+++ Job_Market_Analysis_Api.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
 import urllib, requests, re, pandas as pd
 import simplejson as json
+import regex
 
 app = Flask(__name__)
 
@@ -39,7 +40,21 @@ def get_data(query, num_pages, location):
     print(base_url)
     sort_by = 'date'          # sort by data
     start_from = '&start='    # start page number  
-
+    crimePage = requests.get("https://www.worldatlas.com/articles/most-dangerous-cities-in-canada.html")
+    crimeData = BeautifulSoup(crimePage.text, 'html.parser')
+    table = crimeData.find("table")
+    
+    #output_rows = []
+    output_row = []
+    for table_row in table.findAll('tr'):
+        columns = table_row.findAll('td')
+        
+        for column in columns:
+            output_row.append(column.text)
+            print(column.text)
+            print("*****************")
+            #output_rows.append(output_row)
+    
     for page in range(1,3):
         page = (page-1) * 10  
         # get full url 
@@ -52,8 +67,15 @@ def get_data(query, num_pages, location):
         for div2 in soup1.find_all('div', class_='jobsearch-SerpJobCard unifiedRow row result'):
             job_title_div = div2.find(class_='title')
             job_location_div = div2.find(class_='location accessible-contrast-color-location')
+            job_desc_div = div2.find(class_="summary")
+            # Job Title
             job_title = job_title_div.find('a').text
+            # Job Address
             job_location = job_location_div.text
+            # Job Description
+            job_desc = job_desc_div.text
+            # Job Link
+            job_link = job_title_div.find('a').get('href')
             geolocator = Nominatim(user_agent="your Google Key...")
             location = geolocator.geocode(job_location)
             lng = location.longitude
@@ -61,8 +83,7 @@ def get_data(query, num_pages, location):
             print("Success!")
             # Returns False as x is False 
             x = False
-            print(bool(x))
-            jobDetails = {'lng':lng,'lat':lat,'jobs':job_title,'open':bool(x) }
+            jobDetails = {'lng':lng,'lat':lat,'jobs':job_title,'url':'https://ca.indeed.com/'+job_link,'desc':job_desc,'address':job_location,'crimeData':output_row,'open':bool(x) }
             jobList.append(jobDetails)
             # convert to json data
             jsonStr = json.dumps(jobList)
